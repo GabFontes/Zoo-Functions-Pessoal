f87eb90aa55623c4aa662e4ec3a9409620108004
gab <gabrielfontes321@gmail.com>
2021-10-11 15:05:31 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animals, age) {
   // seu código aqui
+  return data.species.find((specie) => specie.name === animals)
+  .residents.every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,6 +4,6 @@ function getSpeciesByIds(...ids) {
     return data.species.filter((specie) => ids.some((id) => specie.id === id));
 }
 
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46', '0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
